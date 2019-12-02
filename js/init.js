8288f92e867cae29fd96358da2bb17b96f9e768d
muachilin <32566798+muachilin@users.noreply.github.com>
2019-12-02 15:25:54 +0800

    update animation
    update animation

--- js/init.js
+++ js/init.js
@@ -55,3 +55,65 @@ skel.init({
 	
 	
 });
+
+
+
+
+////////Try anime.js
+
+var rolling_ball = document.querySelector(".rolling_ball");
+var playing = false;
+var mousePosition;
+var offset = [0,0];
+var isDown = false;
+
+rolling_ball.addEventListener('click',function() {
+  if(playing)
+    return;
+  playing = true;
+  anime({
+  targets: rolling_ball,
+  //cale: [{value: 1}, {value: 1.4}, {value: 1, delay: 250}],  
+  //rotateY: {value: '+=180', delay: 200},
+  translateX: '+=30',
+  //borderRadius: 100,
+  duration: 400,
+  easing: 'easeInOutSine',
+  //direction: 'alternate',
+    complete: function(anim){
+       playing = false;
+    }
+  //loop: true
+  //autoplay: false
+  //loop: true
+});
+});
+//var rrr=document.getElementsByClassName('rolling_ball');
+//var rr = rrr[0];
+//document.body.appendChild(rolling_ball);
+rolling_ball.addEventListener('mousedown', function(e) {
+    isDown = true;
+    offset = [
+        rolling_ball.offsetLeft - e.clientX,
+        rolling_ball.offsetTop - e.clientY
+    ];
+}, true);
+
+document.addEventListener('mouseup', function() {
+    isDown = false;
+}, true);
+
+document.addEventListener('mousemove', function(event) {
+    event.preventDefault();
+    if (isDown) {
+        mousePosition = { 
+            x : event.clientX,
+            y : event.clientY
+        };
+      
+        rolling_ball.style.left = (mousePosition.x + offset[0]) + 'px';
+        rolling_ball.style.top  = (mousePosition.y + offset[1]) + 'px' ;
+      
+    }
+}, true);
+
